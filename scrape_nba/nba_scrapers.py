19c2ff7e06226580f303057541d0d44f8d0e352b
Franco <francobetteo@gmail.com>
2024-07-09 18:50:57 -0300

    fix path for render

--- scrape_nba/nba_scrapers.py
+++ scrape_nba/nba_scrapers.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import base_scraper.companyscraper
 from selenium import webdriver
 from selenium.webdriver.common.by import By
