9f5c1de0bb47ee613ee0e4611d9bd258d5a8bc08
Franco <francobetteo@gmail.com>
2024-07-24 14:13:46 -0300

    applygin chrome options

--- scrape_f1/f1_scrapers.py
+++ scrape_f1/f1_scrapers.py
@@ -554,10 +554,11 @@ class Williams(base_scraper.companyscraper.CompanyScraper):
 
 chrome_options = Options()
 # required with the current docker image
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=chrome_options)
 teams = [
