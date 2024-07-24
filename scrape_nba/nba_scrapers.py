019ef85cd70d2b2ab634e9e33b5a595f4a3f0a3e
Franco <francobetteo@gmail.com>
2024-07-24 13:10:39 -0300

    typo

--- scrape_nba/nba_scrapers.py
+++ scrape_nba/nba_scrapers.py
@@ -1789,7 +1789,7 @@ chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.addArguments("--disable-gpu")
+chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=chrome_options)
 teams = [
